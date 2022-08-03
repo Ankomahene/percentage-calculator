2e646f9e4cbfffd9db0174969d3c71b962add92a
ankomahene <ankomaheneshadrack@gmail.com>
2022-08-03 17:47:20 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [percentNumber, setPercentNumber] = useState<number>(20);
+  const [actualNumber, setActualNumber] = useState<number>(70);
+  const [results, setResults] = useState(0);
+
+  const handleCalculation = () => {
+    setResults(calculate());
+  };
+
+  useEffect(() => {
+    setResults(calculate());
+
+    // eslint-disable-next-line
+  }, []);
+
+  const calculate = (): number => {
+    return (percentNumber / 100) * actualNumber;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="info">
+          <div className="title">Percentage Calculator</div>
+          <div className="description">Calculate Percentages for free</div>
+        </div>
+        <hr />
+        <div className="main">
+          <div>What is</div>
+          <input
+            type="number"
+            value={percentNumber}
+            onChange={(e) => setPercentNumber(Number(e.target.value))}
+          />
+          <div>% of</div>
+          <input
+            type="number"
+            value={actualNumber}
+            onChange={(e) => setActualNumber(Number(e.target.value))}
+          />
+          <div>?</div>
+        </div>
+        <div className="results">= {results}</div>
+        <button className="calculateBtn" onClick={handleCalculation}>
+          Calculate
+        </button>
+      </div>
     </div>
   );
 }
